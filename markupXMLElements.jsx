5dce4768afd5f37067f88d211a7fa74cdac44552
onebitious <onebitious@icloud.com>
2018-02-18 18:18:47 +0900

    ファイル追加
    「markupXMLElements.jsx」
    「画像配置.jsx」
    上記のファイルを追加

---
+++ markupXMLElements.jsx
@@ -0,0 +1,138 @@
+﻿/*
+大澤作成
+
+入れ子になったフレームは処理しません。
+複数のフレームをグループ化し、入れ子にしたフレームを処理すると「何かしらのエラーが発生しました。」と表示されます。
+例：Illustratorで作成したアウトライン化したロゴをグラフィックフレームに配置しているもの。
+
+★20160309-01
+矩形フレームのオブジェクトの属性が「グラフィック」と「割り当てなし」の場合、
+「グラフィック」→"ItemImage"
+「割り当てなし」→"ItemText"
+とタグを付与するように改修。
+★20160309-02
+多角形フレームにも"ItemImage"が付与されるように改修。
+★20160309-03
+多角形フレームツール、楕円形フレームツールには、"ItemImage"を付与、
+多角形ツール、楕円形ツールには、"ItemText"を付与するように改修。
+★20160310-01
+入れ子になったグループがある場合のエラーメッセージを変更。
+処理を中断するように改修。
+★20160311-01
+処理後、追加したフレームにも処理をするようにエラー処理をコメントアウトしました。
+*/
+
+var myDoc = app.activeDocument;
+
+var myAllPagesItemsLen = myDoc.allPageItems.length; //グループ化しているものも含め総フレーム数を取得
+var myAllPagesItems = myDoc.allPageItems; //グループ化しているものも含め全てのフレームを取得
+//alert(myAllPagesItemsLen);//確認用
+
+try {
+    //▼グループ解除処理
+    try {
+        for (var i = 0; i < myAllPagesItemsLen; i++) {
+            if (myAllPagesItems[i].constructor.name == "Group") {
+                myAllPagesItems[i].ungroup();
+            }
+        }
+    } catch (e) {
+        alert("入れ子になったグループオブジェクトが存在していると思われます。" + "\r" + "\r" + "予期せぬ結果になるので、処理を中断します。");
+        exit();
+    }
+
+    //▼タグを作成
+    try {
+        myDoc.xmlTags.add("PageText", UIColors.MAGENTA); //PegeTextはマゼンタ
+        myDoc.xmlTags.add("ItemImage", UIColors.BLUE); //ItemTextは青
+        myDoc.xmlTags.add("ItemText", UIColors.GREEN); //ItemTextは緑
+        myDoc.xmlTags.add("Spec", UIColors.RED); //Specは赤
+    } catch (e) {
+        //alert("すでに同じ名前のタグが存在します。");
+        // exit();
+    }
+
+    //▼フレームを定義
+    var myTextFramesLen = myDoc.textFrames.length; //テキストフレームの総数
+    var myRectanglesLen = myDoc.rectangles.length; //長方形フレームの総数
+    var myOvalsLen = myDoc.ovals.length; //楕円形フレームの総数
+    var myPolygonLen = myDoc.polygons.length; //多角形フレームの総数
+    var myTextFrames = myDoc.textFrames; //テキストフレーム
+    var myRectangles = myDoc.rectangles; //長方形フレーム
+    var myOvals = myDoc.ovals; //楕円形フレーム
+    var myPolygon = myDoc.polygons; //多角形フレーム
+
+    //フレームの種類によって付与する要素の定義
+    var myRoot = myDoc.xmlElements.item("Root"); //Root
+    var myItemText = myDoc.xmlTags.itemByName("ItemText"); //テキストフレームにItemTextを定義
+    var myItemImage = myDoc.xmlTags.itemByName("ItemImage"); //長方形フレームにItemImageを定義
+
+    //▼フレームにタグ付与処理
+    try {
+        for (i = 0; i < myTextFramesLen; i++) { //テキストフレームの処理
+            myRoot.xmlElements.add(myItemText, myTextFrames[i]); //テキストフレームには"ItemText"を付与
+        }
+    } catch (e) {
+        //alert("このフレームは既にタグ付されています。");
+        //exit();
+    }
+
+    //▼長方形フレームツール、長方形ツールの処理
+    try {
+        for (j = 0; j < myRectanglesLen; j++) {
+            if (myRectangles[j].contentType == ContentType.unassigned) { //「割り当てなし」フレーム
+                myRoot.xmlElements.add(myItemText, myRectangles[j]); //「割り当てなし」フレームには"ItemText"を付与
+            } else {
+                myRoot.xmlElements.add(myItemImage, myRectangles[j]); //その他は"ItemImage"を付与
+            }
+        }
+    } catch (e) {
+        //alert("このフレームは既にタグ付されています。");
+        //exit();
+    }
+
+    //▼楕円形フレームツール、楕円形ツールの処理
+    try {
+        for (k = 0; k < myOvalsLen; k++) {
+            if (myOvals[k].contentType == ContentType.unassigned) { //「割り当てなし」フレーム           
+                myRoot.xmlElements.add(myItemText, myOvals[k]); //楕円形ツール（「割り当てなし」）に"ItemText"を付与
+            } else {
+                myRoot.xmlElements.add(myItemImage, myOvals[k]); //楕円形フレームに"ItemImage"を付与
+            }
+        }
+    } catch (e) {
+        //alert("このフレームは既にタグ付されています。");
+        //exit();
+    }
+
+    //▼多角形フレームツール、多角形ツールの処理
+    try {
+        for (l = 0; l < myPolygonLen; l++) {
+            if (myPolygon[l].contentType == ContentType.unassigned) { //「割り当てなし」フレーム          
+                myRoot.xmlElements.add(myItemText, myPolygon[l]); //多角形ツール（「割り当てなし」）に"ItemText"を付与
+            } else {
+                myRoot.xmlElements.add(myItemImage, myPolygon[l]); //多角形フレームツールに"ItemImage"を付与
+            }
+        }
+    } catch (e) {
+        //alert("このフレームは既にタグ付されています。");
+        //exit();
+    }
+
+    //▼要素に属性名を付与
+    for (var i = 0; i < myRoot.xmlElements.length; i++) {
+        var myElementsName = myRoot.xmlElements[i].markupTag.name.toString();
+        var myElementsObj = myRoot.xmlElements.item(i);
+        try {
+            myElementsObj.xmlAttributes.add("id", "");
+        } catch (e) {
+            //alert("属性はすでに付与されています。");
+            //break;
+        }
+    }
+} catch (e) {
+    //alert("何かしらのエラーが発生しました。" + "\r" + "処理を中断します。");
+    //exit();
+} finally {
+    alert("処理が終了しました。" + "\r" + "\r" + "グループ化は解除されてます。" + "\r" + "ご確認ください。");
+}
---
+++ 画像配置.jsx
@@ -0,0 +1,113 @@
+﻿//▼サイズ指定ウィンドウ表示
+var win = app.dialogs.add({
+    name: 'ドキュメントサイズを選択'
+});
+var dlgClmn = win.dialogColumns.add();
+var btnGrp = dlgClmn.radiobuttonGroups.add();
+var a4Btn = btnGrp.radiobuttonControls.add({
+    staticLabel: 'A4 (210 x 297mm)',
+    minWidth: 150,
+    checkedState: true
+});
+var a3Btn = btnGrp.radiobuttonControls.add({
+    staticLabel: 'A3 (297 x 420mm)',
+    minWidth: 150
+});
+var btnFlg = win.show();
+if (btnFlg == false) {
+    alert("キャンセルしました。");
+    exit();
+}
+if (a4Btn.checkedState == true) {
+    var docWidth = 210;
+    var docHeight = 297;
+} else {
+    var docWidth = 297;
+    var docHeight = 420;
+}
+
+//▼新規ドキュメント作成
+var doc = app.documents.add(); //ドキュメント
+doc.documentPreferences.pageWidth = docWidth; //横サイズ
+doc.documentPreferences.pageHeight = docHeight; //縦サイズ
+doc.documentPreferences.facingPages = false; //単ページ
+doc.viewPreferences.horizontalMeasurementUnits = 2053991795; //水平方向の定規の単位をミリに指定
+doc.viewPreferences.verticalMeasurementUnits = 2053991795; //水垂直方向の定規の単位をミリに指定
+
+try {
+    flag = false;
+    while (flag == false) {
+        var selFolder = Folder.selectDialog("画像の入ったフォルダを選択してください。"); //フォルダを選択
+        if (!selFolder) {
+            alert("キャンセルしました。");
+            doc.close(SaveOptions.no); //保存しないで閉じる
+            exit();
+        } else {
+            var myFiles = selFolder.getFiles(); //ファイルを取得
+            var myFilesLen = myFiles.length; //総ファイル数
+            flag = true;
+        }
+    }
+
+    var myFilesLen = myFiles.length; //総ファイル数
+
+    doc.documentPreferences.pagesPerDocument = myFilesLen; //ファイル数分ページを作成
+    var myPage = doc.pages; //ドキュメント上のページ
+
+//▼プログレッシブバー表示
+var myProDialog = new Window('palette', '処理中...', [800, 480, 1200, 560]);
+myProDialog.center();
+myProDialog.myProgressBar = myProDialog.add("progressbar", [10, 30, 394, 45], 0, 100);
+myProDialog.show();
+
+    //▼ページ数分、グラフィックフレーム作成
+    for (var i = 0, myPageLen = myPage.length; i < myPageLen; i++) {
+        var myRect = myPage[i].rectangles.add(); //長方形を追加
+        myRect.contentType = 1735553140; //グラフィックフレーム
+        myRect.visibleBounds = ['5mm', '5mm', docHeight - 5, docWidth - 5];
+        var myImg = myRect.place(myFiles[i]);
+        myRect.fit(FitOptions.proportionally); //内容をフレーム内に収める
+    }
+} catch (e) {
+    alert("何かしらのエラーが発生しました。\r\n処理するフォルダを選択していない可能性があります。\r\n処理を中断します。");
+    doc.close(SaveOptions.no); //保存しないで閉じる
+    exit();
+}
+
+
+
+//▼ページ数分、画像配置
+for (var j = 0; j < myPageLen; j++) {
+            //▼プログレッシブバーの値
+        var processLength = myFilesLen; //総処理画像数
+        myProDialog.myProgressBar.value += 100 / processLength;
+        
+    var pageObj = myPage[j].pageItems[0].graphics[0]; //ページ（フレーム）内画像
+    var imgX1 = pageObj.visibleBounds[1]; //X1座標
+    var imgY1 = pageObj.visibleBounds[0]; //Y1座標
+    var imgX2 = pageObj.visibleBounds[3]; //X2座標
+    var imgY2 = pageObj.visibleBounds[2]; //Y2座標
+
+    var imgWidth = imgX2 - imgX1; //画像の幅を取得
+    var imgHeighth = imgY2 - imgY1; //画像の高さを取得
+
+    //▼長辺が縦になるよう画像を回転させる
+    try {
+        if (imgWidth > imgHeighth) {
+            pageObj.rotationAngle = 90; //回転
+            pageObj.fit(FitOptions.proportionally); //内容をフレーム内に収める
+        } else {
+            pageObj.fit(FitOptions.proportionally); //内容をフレーム内に収め
+        }
+
+        
+    } catch (e) {
+        alert("何かしらのエラーが発生しました。\r\n処理を中断します。\r\n「この値を入力すると、 1つ以上のオブジェクトがペーストボードから失われる可能性があります。」のエラーかも。。。？");
+        exit;
+    }
+
+        
+}
+//▼プログレスバーを閉じる
+myProDialog.close();
+alert("処理が終わりました。");
